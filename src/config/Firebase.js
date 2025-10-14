28dd47aa6c5f088f8976433de4ecdbb4e43b411f
shahzaib khan <shahzaibshazzo@gmail.com>
2025-10-14 16:42:35 +0500

    all done

--- src/config/Firebase.js
+++ src/config/Firebase.js
@@ -3,17 +3,16 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAU9nGkdu3hDmMOTBHNaxtQUI-6C4dUSxQ",
-  authDomain: "cora-ide.firebaseapp.com",
-  projectId: "cora-ide",
-  storageBucket: "cora-ide.firebasestorage.app",
-  messagingSenderId: "885227904806",
-  appId: "1:885227904806:web:6157aee5ddf1b6de1fabe7",
-  measurementId: "G-SKYWQQNV8D",
-  databaseURL: "https://cora-ide-default-rtdb.firebaseio.com/",
+ apiKey: "AIzaSyAU9nGkdu3hDmMOTBHNaxtQUI-6C4dUSxQ",
+ authDomain: "cora-ide.firebaseapp.com",
+ projectId: "cora-ide",
+ storageBucket: "cora-ide.firebasestorage.app",
+ messagingSenderId: "885227904806",
+ appId: "1:885227904806:web:6157aee5ddf1b6de1fabe7",
+ measurementId: "G-SKYWQQNV8D",
+ databaseURL: "https://cora-ide-default-rtdb.firebaseio.com/",
 };
 
 export const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
- isko env file ke mejn dalke import akrlo
