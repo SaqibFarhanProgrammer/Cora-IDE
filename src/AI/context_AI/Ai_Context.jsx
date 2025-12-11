bc6219a9e5f2717084227c27926aa9340971e5f5
shahzaib887 <shahzaibshazzo@gmail.com>
2025-12-11 21:37:36 +0500

    a

--- src/AI/context_AI/Ai_Context.jsx
+++ src/AI/context_AI/Ai_Context.jsx
@@ -1,4 +1,5 @@
 import { createContext, useState } from "react";
+import { GoogleGenerativeAI } from "@google/generative-ai";
 
 export const AIContext = createContext();
 
@@ -6,7 +7,21 @@ export const AI_Context_provider = ({ children }) => {
   const [expand, setexpand] = useState(true);
   const [close, setclose] = useState(false);
   const [messages, setmessages] = useState([]);
-  const [isAIOpen, setisAIOpen] = useState(true)
+  const [isAIOpen, setisAIOpen] = useState(true);
+
+  const ai = new GoogleGenerativeAI(import.meta.env.VITE_AI_KEY);
+
+  async function run() {
+    const model = ai.getGenerativeModel({ model: "gemini-2.5-flash" });
+
+    const result = await model.generateContent(
+      "Explain js"
+    );
+
+    console.log(result.response.text());
+  }
+
+  run();
 
   const value = {
     expand,
@@ -16,7 +31,7 @@ export const AI_Context_provider = ({ children }) => {
     messages,
     setmessages,
     isAIOpen,
-    setisAIOpen
+    setisAIOpen,
   };
 
   return <AIContext.Provider value={value}>{children}</AIContext.Provider>;
